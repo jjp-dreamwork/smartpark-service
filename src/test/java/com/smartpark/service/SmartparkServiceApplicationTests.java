6171954197330606d83826bccba323ae20d12fe6
JJ Pulido <john.jason.pulido@gmail.com>
2026-07-15 02:03:19 +0800

    initial commit: initialize spring boot maven project structure

---
+++ src/test/java/com/smartpark/service/SmartparkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartpark.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartparkServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
